16b119e4a28fe566804eede3e59b0eea79528e07
Louis Ng <louisng1@hotmail.com>
2018-05-08 17:45:48 -0700

    page3 and page4

--- js/spoonpage3.js
+++ js/spoonpage3.js
@@ -1,4 +1,4 @@
-
+/*
 var data = JSON.parse(sessionStorage.getItem('farray'));
 console.log(data);
 
@@ -31,33 +31,23 @@ $.ajax(settings).done(function (response) {
   console.log(response);
     console.log(JSON.stringify(response));
     var listData = "testing";
-    listData += response[0].id;
-$("#recipe").append(listData);
+        for(var key in response){
+    var a = document.createElement("a");
+    a.setAttribute("href","#");
+    a.setAttribute("onclick", "sendId(this.id)");
+    a.setAttribute("id", "" + response[key].id);
+    a.innerHTML = response[key].title + "<br/>";
+    $("#recipe").append(a);
+    }
+
     
 });
-/*
-var settings2 = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/556470/information?includeNutrition=false",
-  "method": "GET",
-  "headers": {
-    "X-Mashape-Key": "BRCCNUxTIWmshvCO6klV6Hi0HVFsp1AqP3wjsnHTd2TJd7xljC",
-    "Accept": "application/json",
-    "Cache-Control": "no-cache",
-    "Postman-Token": "40fda88d-ab1a-42ea-8314-999f003203c0"
-  }
-}
 
-$.ajax(settings2).done(function (response2) {
-  console.log(response2);
-    var listData ="Recipe <br/>"; 
-    for(var key in response2["extendedIngredients"]) {
-        listData += response2.extendedIngredients[key].originalString + "<br/>";
-
-    } 
-    listData +=  response2.title + "<br/>" 
-    + response2.instructions;
-    $("#recipe").append(listData);
-});
+function sendId(clicked_id) {
+    sessionStorage.setItem('foodId', clicked_id);
+    var idparam = "storage" + sessionStorage.getItem('foodId');
+    console.log(idparam);
+    location.href = "page4.html";
+    
+};
 */
