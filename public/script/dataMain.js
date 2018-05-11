939ddbe4b38eac22c093845acb4a4629f7da5d6e
yonghwan <geniushwan5263@gmail.com>
2018-05-11 11:02:10 -0700

    Fixed searching

--- public/script/dataMain.js
+++ public/script/dataMain.js
@@ -357,13 +357,15 @@ function autocomplete(inp, arr) {
                 /*create a DIV element for each matching element:*/
                 b = document.createElement("DIV");
                 /*make the matching letters bold:*/
-                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-                b.innerHTML += arr[i].substr(val.length);
+                b.innerHTML = "<strong>" + arr[i].name.substr(0, val.length) + "</strong>";
+                b.innerHTML += arr[i].name.substr(val.length);
                 /*insert a input field that will hold the current array item's value:*/
-                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                b.innerHTML += "<input type='hidden' value='" + arr[i].name + "'>";
                 /*execute a function when someone clicks on the item value (DIV element):*/
                 b.addEventListener("click", function (e) {
                     /*insert the value for the autocomplete text field:*/
+                    // inp.value = this.getElementsByTagName("input")[0].name;
+                    console.log(this.getElementsByTagName("input"));
                     inp.value = this.getElementsByTagName("input")[0].value;
                     /*close the list of autocompleted values,
                     (or any other open lists of autocompleted values:*/
